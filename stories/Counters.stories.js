edfd594c4c91827b4de0fb8e9f8791b1e7cf5d5c
Tim Case <tim@2drops.net>
2021-06-06 13:47:16 -0700

    Remove console log

--- stories/Counters.stories.js
+++ stories/Counters.stories.js
@@ -6,7 +6,6 @@ export const elm = () => {
  const container = document.createElement('div');
  const elmContainer = document.createElement('div');
  container.appendChild(elmContainer);
-  console.log(elmContainer);
  const app = CountersStory.Elm.Counters.init({
    node: elmContainer,
    flags: 5
